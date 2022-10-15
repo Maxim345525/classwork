9aa7d3f7453461fd2c05251c0907f9f64cb9963e
msxim_m <maximmaksimchyk@gmail.com>
2022-10-15 11:37:26 +0300

    add code

---
+++ ex_1.py
@@ -0,0 +1,16 @@
+import sys
+
+try:
+    num_1 = int(input("Enter size"))
+    for width in range(num_1):
+        for long in range(num_1):
+            if width == 0 or long == 0 or width == num_1 - 1 or long == num_1 -1:
+                sys.stdout.write("*")
+            else:
+                sys.stdout.write("")
+        print('')
+except Exception as err:
+    print(f'Error: {err}')
+finally:
+    print("exit")
+
