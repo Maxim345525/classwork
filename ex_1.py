9d74ba8f8f9adc9a3a95fa90ea9c3b6efa3b3de6
msxim_m <maximmaksimchyk@gmail.com>
2022-10-15 10:47:23 +0300

    add code

---
+++ ex_1.py
@@ -0,0 +1,12 @@
+try:
+    vel=int(input("Введіть розмір квадрату"))
+    for i in range(vel):
+        for j in range(vel):
+            print(f'*', end=" ")
+        print()
+except Exception as ex:
+    print(f"error:{ex}")
+finally:
+    print("exit")
+
+
